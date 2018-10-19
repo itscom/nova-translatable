0fec825d2ece41a30bdd294ccbef14537f6e63e1
Angelo Alfano <angeloalfano@gmail.com>
2018-10-19 14:30:55 +0200

    fixed create/edit locale

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -2,18 +2,18 @@
     <panel-item :field="field">
         <template slot="value">
 
-            <a 
-                class="inline-block font-bold cursor-pointer mr-2 animate-text-color select-none" 
-                :class="{ 'text-60': localeKey !== currentLocale, 'text-primary border-b-2': localeKey === currentLocale }"
-                :key="`a-${localeKey}`" 
-                v-for="(locale, localeKey) in field.locales"
-                @click="changeTab(localeKey)"
+            <a
+                    class="inline-block font-bold cursor-pointer mr-2 animate-text-color select-none"
+                    :class="{ 'text-60': localeKey !== currentLocale, 'text-primary border-b-2': localeKey == currentLocale }"
+                    :key="`a-${localeKey}`"
+                    v-for="(locale, localeKey) in field.locales"
+                    @click="changeTab(localeKey)"
             >
                 {{ locale }}
             </a>
 
             <div class="mt-4">
-                <span v-if="field.value[currentLocale]" v-html="field.value[currentLocale]"></span>
+                <span v-if="field.value[currentLocaleCode]" v-html="field.value[currentLocaleCode]"></span>
                 <span v-else>—</span>
             </div>
 
@@ -22,19 +22,37 @@
 </template>
 
 <script>
-export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
+    import {EventBus} from '../event-bus';
 
-    data() {
-        return {
-            currentLocale: Object.keys(this.field.locales)[0]
-        }
-    },
+    export default {
+        props: ['resource', 'resourceName', 'resourceId', 'field'],
 
-    methods: {
-        changeTab(locale) {
-            this.currentLocale = locale
+        data() {
+            return {
+                currentLocale: Object.keys(this.field.locales)[0],
+                currentLocaleCode: Object.values(this.field.locales)[0]
+            }
+        },
+        mounted() {
+            EventBus.$on('localeChanged', locale => {
+                if (this.currentLocale !== locale) {
+                    this.changeTab(locale, true);
+                }
+            });
+        },
+        methods: {
+            changeTab(locale, dontEmit) {
+                if (this.currentLocale !== locale) {
+                    if (!dontEmit) {
+                        EventBus.$emit('localeChanged', locale);
+                    }
+                    this.changeLocale(locale);
+                }
+            },
+            changeLocale(locale) {
+                this.currentLocale = locale;
+                this.currentLocaleCode = Object.values(this.field.locales)[locale];
+            }
         }
     }
-}
 </script>
--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,48 +1,48 @@
 <template>
     <default-field :field="field">
         <template slot="field">
-            <a 
-                class="inline-block font-bold cursor-pointer mr-2 animate-text-color select-none" 
-                :class="{ 'text-60': localeKey !== currentLocale, 'text-primary border-b-2': localeKey === currentLocale }"
-                :key="`a-${localeKey}`" 
-                v-for="(locale, localeKey) in field.locales"
-                @click="changeTab(localeKey)"
+            <a
+                    class="inline-block font-bold cursor-pointer mr-2 animate-text-color select-none"
+                    :class="{ 'text-60': localeKey !== currentLocale, 'text-primary border-b-2': localeKey == currentLocale }"
+                    :key="`a-${localeKey}`"
+                    v-for="(locale, localeKey) in field.locales"
+                    @click="changeTab(localeKey)"
             >
                 {{ locale }}
             </a>
 
             <textarea
-                ref="field" 
-                :id="field.name"
-                class="mt-4 w-full form-control form-input form-input-bordered py-3 min-h-textarea"
-                :class="errorClasses"
-                :placeholder="field.name"
-                v-model="value[currentLocale]"
-                v-if="!field.singleLine && !field.trix"
-                @keydown.tab="handleTab"
+                    ref="field"
+                    :id="field.name"
+                    class="mt-4 w-full form-control form-input form-input-bordered py-3 min-h-textarea"
+                    :class="errorClasses"
+                    :placeholder="field.name"
+                    v-model="value[currentLocaleCode]"
+                    v-if="!field.singleLine && !field.trix"
+                    @keydown.tab="handleTab"
             ></textarea>
 
             <div v-if="!field.singleField && field.trix" class="mt-4">
                 <trix
-                    ref="field"
-                    name="trixman"
-                    :value="value[currentLocale]"
-                    placeholder=""
-                    @change="handleChange"
-                    
+                        ref="field"
+                        name="trixman"
+                        :value="value[currentLocaleCode]"
+                        placeholder=""
+                        @change="handleChange"
+
                 />
             </div>
 
-            <input 
-                ref="field" 
-                type="text" 
-                :id="field.name"
-                class="mt-4 w-full form-control form-input form-input-bordered"
-                :class="errorClasses"
-                :placeholder="field.name"
-                v-model="value[currentLocale]"
-                v-if="field.singleLine"
-                @keydown.tab="handleTab"
+            <input
+                    ref="field"
+                    type="text"
+                    :id="field.name"
+                    class="mt-4 w-full form-control form-input form-input-bordered"
+                    :class="errorClasses"
+                    :placeholder="field.name"
+                    v-model="value[currentLocaleCode]"
+                    v-if="field.singleLine"
+                    @keydown.tab="handleTab"
             />
 
             <p v-if="hasError" class="my-2 text-danger">
@@ -54,91 +54,94 @@
 
 <script>
 
-import Trix from '../Trix'
+    import Trix from '../Trix'
 
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
-import { EventBus } from '../event-bus';
+    import {FormField, HandlesValidationErrors} from 'laravel-nova'
+    import {EventBus} from '../event-bus';
 
-export default {
-    mixins: [FormField, HandlesValidationErrors],
+    export default {
+        mixins: [FormField, HandlesValidationErrors],
 
-    props: ['resourceName', 'resourceId', 'field'],
+        props: ['resourceName', 'resourceId', 'field'],
 
-    components: { Trix },
+        components: {Trix},
 
-    data() {
-        return {
-            locales: Object.keys(this.field.locales),
-            currentLocale: null,
-        }
-    },
-
-    mounted() {
-        this.currentLocale = this.locales[0] || null;
-
-        EventBus.$on('localeChanged', locale => {
-            if(this.currentLocale !== locale) {
-                this.changeTab(locale, true);
+        data() {
+            return {
+                locales: Object.keys(this.field.locales),
+                currentLocale: null, // an index
+                currentLocaleCode: null, // an iso 8859-1 code
             }
-        });
-    },
-
-    methods: {
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-          this.value = this.field.value || {}
         },
 
-        /**
-         * Fill the given FormData object with the field's internal value.
-         */
-        fill(formData) {
-            Object.keys(this.value).forEach(locale => {
-                formData.append(this.field.attribute + '[' + locale + ']', this.value[locale] || '')
-            })
-        },
-
-        /**
-         * Update the field's internal value.
-         */
-        handleChange(value) {
-          this.value[this.currentLocale] = value
-        },
-
-        changeTab(locale, dontEmit) {
-            if(this.currentLocale !== locale){
-                if(!dontEmit){
-                    EventBus.$emit('localeChanged', locale);
+        mounted() {
+            this.changeTab(this.locales[0] || null, true);
+            EventBus.$on('localeChanged', locale => {
+                if (this.currentLocale !== locale) {
+                    this.changeTab(locale, true);
                 }
+            });
+        },
 
-                this.currentLocale = locale;
+        methods: {
+            /*
+             * Set the initial, internal value for the field.
+             */
+            setInitialValue() {
+                this.value = this.field.value || {}
+            },
 
-                this.$nextTick(() => {
-                    if (this.field.trix) {
-                        this.$refs.field.update()
-                    } else {
-                        this.$refs.field.focus()
-                    }
+            /**
+             * Fill the given FormData object with the field's internal value.
+             */
+            fill(formData) {
+                Object.keys(this.value).forEach(locale => {
+                    formData.append(this.field.attribute + '[' + locale + ']', this.value[locale] || '')
                 })
-            }
-        },
+            },
 
-        handleTab(e) {
-            const currentIndex = this.locales.indexOf(this.currentLocale)
-            if (!e.shiftKey) {
-                if (currentIndex < this.locales.length - 1) {
-                    e.preventDefault();
-                    this.changeTab(this.locales[currentIndex + 1]);
+            /**
+             * Update the field's internal value.
+             */
+            handleChange(value) {
+                this.value[this.currentLocaleCode] = value
+            },
+
+            changeTab(locale, dontEmit) {
+                console.log(locale);
+                if (this.currentLocale !== locale) {
+                    if (!dontEmit) {
+                        EventBus.$emit('localeChanged', locale);
+                    }
+                    this.changeLocale(locale);
+
+                    this.$nextTick(() => {
+                        if (this.field.trix) {
+                            this.$refs.field.update()
+                        } else {
+                            this.$refs.field.focus()
+                        }
+                    })
                 }
-            } else {
-                if (currentIndex > 0) {
-                    e.preventDefault();
-                    this.changeTab(this.locales[currentIndex - 1]);
+            },
+            changeLocale(locale) {
+                this.currentLocale = locale;
+                this.currentLocaleCode = this.field.locales[locale];
+            },
+            handleTab(e) {
+                const currentIndex = this.locales.indexOf(this.currentLocale);
+                if (!e.shiftKey) {
+                    if (currentIndex < this.locales.length - 1) {
+                        e.preventDefault();
+                        this.changeTab(this.locales[currentIndex + 1]);
+                    }
+                } else {
+                    if (currentIndex > 0) {
+                        e.preventDefault();
+                        this.changeTab(this.locales[currentIndex - 1]);
+                    }
                 }
             }
         }
     }
-}
 </script>
